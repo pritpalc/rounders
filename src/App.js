999d08ba60122d7d582840466623ac1becce7364
rubenprograms <gonsalespalazio@gmail.com>
2020-06-05 23:03:37 -0700

    Created CreateChallenge component, a main component for our application.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  ThemeProvider,
+  createMuiTheme
+} from '@material-ui/core';
+// Components
+import CreateChallenge from './components/CreateChallenge';
+// Style
 import './App.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#6202EE",
+    },
+    secondary: {
+      main: "#A32F69"
+    }
+  },
+  typography: {
+    h1: {
+      fontSize: "4rem"
+    },
+    h2: {
+      fontSize: "3.5rem"
+    },
+    h3: {
+      fontSize: "3rem"
+    },
+    h4: {
+      fontSize: "2.5rem"
+    },
+    h5: {
+      fontSize: "2rem"
+    },
+    h6: {
+      fontSize: "1.5rem"
+    },
+    button: {
+      fontWeight: "600",
+      letterSpacing: "2.83px",
+      fontSize: "0.95rem",
+      lineHeight: "1.7rem",
+      textTransform: "uppercase"
+    }
+  },
+  shape: {
+    borderRadius: 8
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div id="App">
+        <CreateChallenge />
+      </div>
+    </ThemeProvider>
   );
 }
 
